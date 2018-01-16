ee6182a6eb8b99d3028339b6c7ddfe525c84b3de
Hector Sanjuan <code@hector.link>
2018-01-16 11:53:30 +0100

    Feat #277: Fix some tests
    License: MIT
    Signed-off-by: Hector Sanjuan <code@hector.link>

--- cluster_test.go
+++ cluster_test.go
@@ -198,7 +198,11 @@ func TestClusterPin(t *testing.T) {
 
 	// test an error case
 	cl.consensus.Shutdown()
-	err = cl.Pin(api.PinCid(c))
+	err = cl.Pin(api.Pin{
+		Cid:                  c,
+		ReplicationFactorMax: 1,
+		ReplicationFactorMin: 1,
+	})
 	if err == nil {
 		t.Error("expected an error but things worked")
 	}
